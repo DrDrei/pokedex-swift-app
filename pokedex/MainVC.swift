485e4fa9071c6c9739c305e80cd781f8f3df9e01
Andrei <drei3000@gmail.com>
2016-01-25 16:29:50 -0700

    Added detailview segue, collectionview done

--- pokedex/MainVC.swift
+++ pokedex/MainVC.swift
@@ -8,35 +8,68 @@
 
 import UIKit
 
-class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 	
 	@IBOutlet weak var collectionView: UICollectionView!
 	@IBOutlet weak var searchBar: UISearchBar!
-	var pokemon = [Pokemon]()
+	var unfilteredPokemon = [Pokemon]()
+	var filteredPokemon = [Pokemon]()
+	var inSearchMode = false
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
 		collectionView.delegate = self
 		collectionView.dataSource = self
+		searchBar.delegate = self
+		searchBar.returnKeyType = UIReturnKeyType.Done
+		
+		
 		parseCSV()
 	}
 	
 	// MARK: Collection View Functions
 	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
 		if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("pokemonCell", forIndexPath: indexPath) as? PokemonCell {
-			let poke = pokemon[indexPath.row]
-			cell.configureCell(poke)
+			let pokemon: Pokemon!
+			if inSearchMode {
+				pokemon = filteredPokemon[indexPath.row]
+			} else {
+				pokemon = unfilteredPokemon[indexPath.row]
+			}
+			
+			cell.configureCell(pokemon)
 			return cell
 		}
 		return UICollectionViewCell()
 	}
 	
 	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 718
+		if inSearchMode {
+			return filteredPokemon.count
+		}
+		return unfilteredPokemon.count
 	}
 	
 	func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+		var pokemon: Pokemon!
+		if inSearchMode {
+			pokemon = filteredPokemon[indexPath.row]
+		} else {
+			pokemon = unfilteredPokemon[indexPath.row]
+		}
 		
+		performSegueWithIdentifier("pokemonDetailVC", sender: pokemon!)
+	}
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "pokemonDetailVC" {
+			if let detailVC = segue.destinationViewController as? PokemonDetailVC {
+				if let pokemon = sender as? Pokemon {
+					detailVC.pokemon = pokemon
+				}
+			}
+		}
 	}
 	
 	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -47,6 +80,10 @@ class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
 		return CGSizeMake(90, 90)
 	}
 	
+	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		view.endEditing(true)
+		collectionView.endEditing(true)
+	}
 	
 	func parseCSV() {
 		let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
@@ -54,11 +91,27 @@ class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
 			let csv = try CSV(contentsOfURL: path)
 			let rows = csv.rows
 			for row in rows {
-				pokemon.append(Pokemon(name: row["identifier"]!, pokedexID: Int(row["id"]!)!))
+				unfilteredPokemon.append(Pokemon(name: row["identifier"]!, pokedexID: Int(row["id"]!)!))
 			}
 		} catch let error as NSError {
 			print(error.debugDescription)
 		}
+	}
+	
+	func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+		view.endEditing(true)
+	}
+	
+	func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+		if searchBar.text == nil || searchBar.text == "" {
+			inSearchMode = false
+			view.endEditing(true)
+		} else {
+			inSearchMode = true
+			let searchWord = searchBar.text!.lowercaseString
+			filteredPokemon = unfilteredPokemon.filter({$0.name.rangeOfString(searchWord) != nil})
+		}
+		collectionView.reloadData()
 	}
 	
 }
---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Drei on 2016-01-25.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+	var pokemon: Pokemon!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
